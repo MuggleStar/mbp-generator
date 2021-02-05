dda8e9cf167eb1737f8fbbdd0d65e232252e75ce
Madison <jianrong@datastory.com.cn>
2021-02-05 11:05:08 +0800

    commit

--- src/test/java/tests/GeneratorTest.java
+++ src/test/java/tests/GeneratorTest.java
@@ -16,15 +16,14 @@ public class GeneratorTest {
         DataSourceConfig dataSourceConfig = new DataSourceConfig();
 
         dataSourceConfig.setDriverName("com.mysql.cj.jdbc.Driver");
-        dataSourceConfig.setUrl("jdbc:mysql://192.168.0.104:3306/generator?useUnicode=true&serverTimezone=GMT&useSSL=false&characterEncoding=utf8");
+        dataSourceConfig.setUrl("jdbc:mysql://mysql.tenet.com:3306/tenet-backend?useUnicode=true&serverTimezone=GMT&useSSL=false&characterEncoding=utf8");
         dataSourceConfig.setUsername("root");
         dataSourceConfig.setPassword("root");
         GeneratorHelp generatorHelp = new GeneratorHelp(dataSourceConfig);
         generatorHelp.setAuthor("Madison");
         generatorHelp.setPackageName("com.mugglestar");
 
-        generatorHelp.execute("order","t_order","t_");
-        generatorHelp.execute("order","t_order_item","t_");
+        generatorHelp.execute("bcMemu","bc_menu","");
 
     }
 
